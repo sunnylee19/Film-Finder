6b869cb96323943e963d7bdd58d0e22ca579781e
Jason Lu <jasonlu6@bu.edu>
2020-03-04 19:32:52 -0500

    Added in results component.

--- film-finder/src/App.js
+++ film-finder/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import HomePageComponent from "./components/HomePageComponent";
 import SignInPageComponent from "./components/SignInPageComponent";
 import UserPageComponent from "./components/UserPageComponent";
+import ResultsComponent from "./components/ResultsComponent";
 
 const App = () =>
     //<SignInPageComponent/>;
     //<HomePageComponent/>;
-    <UserPageComponent/>
-
+    //<UserPageComponent/>;
+    <ResultsComponent/>;
 
 export default App;
