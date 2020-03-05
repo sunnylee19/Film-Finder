cacf2be9fcd8f71718820ffd52ac5663403d9a15
Jason Lu <jasonlu6@bu.edu>
2020-03-04 19:33:14 -0500

    Added in constants for the Film Finder app.

--- film-finder/src/common/constants.js
+++ film-finder/src/common/constants.js
@@ -1,4 +1,5 @@
 /* hardcoded for now, since we do not have private API key for prototype yet. */
 
-OMDB_MOVIES_URL = 'http://www.omdbapi.com/?apikey=[yourkey]&'
+export const OMDB_MOVIES_URL = 'http://www.omdbapi.com/?apikey=[yourkey]&'
 
+export const MOVIE_URL_SPIDERMAN = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FSpider-Man%3A_Far_From_Home&psig=AOvVaw1ZYTmQKR_KHKIZyiENFNOT&ust=1583451951978000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCOjSh6uAgugCFQAAAAAdAAAAABAD'
