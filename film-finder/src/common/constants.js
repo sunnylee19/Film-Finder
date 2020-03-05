eabe9a5cba780b693ff50ec932863b7ac4e95d3d
Jason Lu <jasonlu6@bu.edu>
2020-03-04 19:26:01 -0500

    No change for the package-lock.json

---
+++ film-finder/src/common/constants.js
@@ -0,0 +1,4 @@
+/* hardcoded for now, since we do not have private API key for prototype yet. */
+
+OMDB_MOVIES_URL = 'http://www.omdbapi.com/?apikey=[yourkey]&'
+
---
+++ film-finder/src/components/ResultsListItemComponent.js
@@ -0,0 +1,9 @@
+import React from "react"
+import {faPencilAlt, faSave, faTrash} from "@fortawesome/free-solid-svg-icons"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+class ResultListItemComponent extends React.Component {
+
+}
+
+export default ResultListItemComponent
