426a4469c3761733a2ed7e35cff968a9988d2cb6
Jason Lu <jasonlu6@bu.edu>
2020-03-10 18:56:55 -0400

    The movie results are now dynamic, without CRUD operation buttons.

--- film-finder/src/components/results/ResultsGridItemComponent.js
+++ film-finder/src/components/results/ResultsGridItemComponent.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 export default ({movie}) => (
-    <div class="col-xs-12 col-sm-6 col-md-3 card">
-        <img src={movie.posterUrl}/>
-        <div className="wbdv-movie-title">
-            {movie.title}
+    <div className="row">
+        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 card">
+            <img
+                src="https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg"/>
+
+
+            <div className={"wbdv-movie-title"} id={"spiderman"}>
+                Spiderman: Far From Home (2019)
+            </div>
+
+            Best Stream: <a href="https://www.netflix.com/">Netflix</a>
+
+            <button className={"wbdv-details-button btn btn-default"}>
+                Details
+            </button>
         </div>
-
-        Best Stream: <a href="https://www.netflix.com/">Netflix</a>
-
-        <button className={"wbdv-details-button btn btn-default"}>
-            Details
-        </button>
     </div>
 );
