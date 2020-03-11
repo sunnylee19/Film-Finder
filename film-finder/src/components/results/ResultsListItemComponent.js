f2dc2854576c7e314c9697e0a82c87fd0e728a56
Jason Lu <jasonlu6@bu.edu>
2020-03-10 22:31:51 -0400

    Added border to table list view for movie item results.

--- film-finder/src/components/results/ResultsListItemComponent.js
+++ film-finder/src/components/results/ResultsListItemComponent.js
@@ -13,8 +13,8 @@ class ResultsListItemComponent extends React.Component {
                 {/* <li className={`list-group-item ${this.state.active?'active':''}`}> */}
                 <li>
                     <div class="row">
-                        <div class="col-12">
-                            <div class="col-12">
+                        <div class="col-12 row">
+                            <div class="col-12 wbdv-list-item-border">
 
                                 <div className={"wbdv-movie-title"} id={"spiderman"}>
                                     <h3 className={"wbdv-movie-title-item"}>Spiderman: Far From Home (2019)</h3>
@@ -29,15 +29,15 @@ class ResultsListItemComponent extends React.Component {
                                 <ol>Last Modified: March 04 2020</ol>
                                 <ol>Rating: PG-13</ol>
                                 <ol>Duration: 2 Hours 9 Minutes</ol>
+                            </div>
+                            </div>
 
+                                <div className="col-12 wbdv-list-item-border">
                                 <div className={"wbdv-movie-title"} id={"spiderman"}>
                                     <h3 className={"wbdv-movie-title-item"}> Joker (2020)</h3>
                                 </div>
 
                                 Best Stream: <a href="https://www.netflix.com/">Netflix</a>
-                            </div>
-
-                            </div>
 
                             <div className="col-5 wbdv-table-view">
                                 <button className={"wbdv-details-button btn btn-default"}>
@@ -47,6 +47,7 @@ class ResultsListItemComponent extends React.Component {
                                 <ol>Rating: R</ol>
                                 <ol>Duration: 2 hours 10 minutes</ol>
                             </div>
+                        </div>
                         </div>
                     </div>
                 </li>
