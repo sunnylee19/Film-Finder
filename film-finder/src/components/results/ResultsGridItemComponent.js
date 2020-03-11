a1eaf2454d8d455889ab1d3a23c97365075f1329
Jason Lu <jasonlu6@bu.edu>
2020-03-10 22:23:53 -0400

    Changed the image resizing to only results.

--- film-finder/src/components/results/ResultsGridItemComponent.js
+++ film-finder/src/components/results/ResultsGridItemComponent.js
@@ -5,6 +5,7 @@ export default ({movie}) => (
     <div className="row">
         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 card">
             <img
+                className={"wbdv-results"}
                 src="https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg"
                 width={"200px"}
                 height={"200px"}/>
@@ -23,6 +24,7 @@ export default ({movie}) => (
 
         <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 card">
             <img
+                className={"wbdv-results"}
                 src="https://upload.wikimedia.org/wikipedia/en/e/e1/Joker_%282019_film%29_poster.jpg"
                 width={"200px"}
                 height={"200px"}/>
