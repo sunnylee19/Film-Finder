5a445cd90e3a5e8c7715b367395310a110128c22
bachung <brandon.chung@vism.co>
2020-03-11 17:18:58 -0400

    ADded pagination

---
+++ film-finder/src/components/results/PaginationComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import query from '../../common/query';
+import {useLocation} from 'react-router-dom';
+
+export default ({numPages, page}) => {
+    const queryParams = new URLSearchParams(useLocation().search);
+    const pageComponents = [];
+    for (let i = 1; i <= numPages; i++) {
+        let classes = ["page-item"];
+        if (i === page) {
+            classes.push("active");
+        }
+        pageComponents.push(
+            <li className={classes.join(" ")} key={i}>
+                <a className="page-link" href={query('/movies', {s: queryParams.get('s'), page: i})}>{i}<span className="sr-only">(current)</span></a>
+            </li>
+        );
+    }
+    return (
+        <ul class="pagination">
+            {pageComponents}
+        </ul>
+    );
+};
