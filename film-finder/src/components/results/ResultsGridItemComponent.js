ab6e64f399c9779f5b3fdebccd1010cfccb5fb83
Jason Lu <jasonlu6@bu.edu>
2020-03-10 23:05:51 -0400

    Fixed the off format for boxes in movie result item images.

--- film-finder/src/components/results/ResultsGridItemComponent.js
+++ film-finder/src/components/results/ResultsGridItemComponent.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../../css/movie-card-css.css'
 
+// try W3 schools card:
+// source: https://www.w3schools.com/w3css/w3css_cards.asp
+
 export default ({movie}) => (
-    <div className="row">
-        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 card">
+    <div className="row w3-container">
+        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 w3-card-4">
             <img
                 className={"wbdv-results"}
                 src="https://upload.wikimedia.org/wikipedia/en/b/bd/Spider-Man_Far_From_Home_poster.jpg"
-                width={"200px"}
-                height={"200px"}/>
+                width={"100px"}
+                height={"100px"}/>
 
 
-            <div className={"wbdv-movie-title"} id={"spiderman"}>
+            <div className={"wbdv-movie-title w3-container w3-center"} id={"spiderman"}>
                 Spiderman: Far From Home (2019)
             </div>
 
@@ -22,16 +25,35 @@ export default ({movie}) => (
             </button>
         </div>
 
-        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 card">
+        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 w3-card-4">
             <img
                 className={"wbdv-results"}
                 src="https://upload.wikimedia.org/wikipedia/en/e/e1/Joker_%282019_film%29_poster.jpg"
-                width={"200px"}
-                height={"200px"}/>
+                width={"100px"}
+                height={"100px"}/>
 
 
-            <div className={"wbdv-movie-title"} id={"spiderman"}>
+            <div className={"wbdv-movie-title w3-container w3-center"} id={"spiderman"}>
                 Joker (2020)
+            </div>
+
+            Best Stream: <a href="https://www.netflix.com/">Netflix</a>
+
+            <button className={"wbdv-details-button btn btn-default"}>
+                Details
+            </button>
+        </div>
+
+        <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 w3-card-4">
+            <img
+                className={"wbdv-results"}
+                src="https://upload.wikimedia.org/wikipedia/en/4/4f/Frozen_2_poster.jpg"
+                width={"100px"}
+                height={"100px"}/>
+
+
+            <div className={"wbdv-movie-title w3-container w3-center"} id={"spiderman"}>
+                Frozen II (2019)
             </div>
 
             Best Stream: <a href="https://www.netflix.com/">Netflix</a>
