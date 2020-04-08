c6c813681ee35ec2cbfe0611e7d7233ce21eded4
Ming-Shan (Sunny) Lee <sunnysmithie@gmail.com>
2020-04-08 22:41:36 +0800

    update profile, change from export to class with API, how to display user detail when not logged in when we don't have user details

--- film-finder/src/components/profile/MyUserDetailsComponent.js
+++ film-finder/src/components/profile/MyUserDetailsComponent.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import '../../css/sign-in-css.css';
 import withUser from '../../common/withUser';
-import { logout } from '../../services/UserService';
 
 export default withUser(props => {
 
-
     const {user} = props;
+    let state;
+    state = {
+        isEditing: true,
+        updatedFirstName: '',
+        updatedLastName: '',
+        updatedEmail: '',
+        updatedPassword: '',
+        updatedDob: ''
 
-    const logoutHandler = async () => {
-        await logout();
-        props.setUser(null);
     };
-
 
     return (
 
@@ -21,40 +23,82 @@ export default withUser(props => {
                 <img className="user-photo" src="https://avatars0.githubusercontent.com/u/1682522?s=400&u=5616079a6e3baa83b765695adce7515ac1749114&v=4" alt="Card image cap"/>
                 <div className="card-body">
 
-                    <h5 className="card-title text-center">
-                        Jose
-
+                    {user &&
+                     <h5 className="card-title text-center">
+                        {user.firstName}
                     </h5>
+                    }
 
 
                     <form>
                         {user &&
-                        <div className="form-group row">
-                            <label htmlFor="colFormLabelSm"
-                                   className="col-sm-4 col-form-label col-form-label-sm">
-                                Email
+                         <div className="form-group row">
+                             <label htmlFor="colFormLabelSm"
+                                    className="col-sm-4 col-form-label col-form-label-sm">
+                                 First Name
+                             </label>
+                              <div className="col-sm-8">
+                                  {!state.isEditing &&
+                                   <input readOnly type="text" className="form-control form-control-sm"
+                                          id="colFormLabelSm" placeholder={user.firstName}/>
+                                  }
+                                  {state.isEditing &&
+                                   <input type="text" className="form-control form-control-sm"
+                                          id="colFormLabelSm" placeholder={user.firstName}/>}
 
-                            </label>
-                            {user &&
-                            <div className="col-sm-8">
-                                <input readOnly type="email" className="form-control form-control-sm"
-                                       id="colFormLabelSm" placeholder={user.email}/>
-                            </div>}
-                        </div>
+                              </div>
+                         </div>
                         }
                         {user &&
-                            <div className="form-group row">
-                                <label htmlFor="colFormLabelSm"
-                                       className="col-sm-4 col-form-label col-form-label-sm">
-                                    Password
-                                </label>
-                                <div className="col-sm-8">
-                                    <input readOnly type="text"
-                                           className="form-control form-control-sm"
-                                           id="colFormLabelSm" placeholder={user.password}/>
-                                </div>
-                            </div>
+                         <div className="form-group row">
+                             <label htmlFor="colFormLabelSm"
+                                    className="col-sm-4 col-form-label col-form-label-sm">
+                                 Last Name
+                             </label>
+                              <div className="col-sm-8">
+                                  {!state.isEditing &&
+                                   <input readOnly type="text" className="form-control form-control-sm"
+                                          id="colFormLabelSm" placeholder={user.lastName}/>}
+                                  {state.isEditing &&
+                                   <input type="text" className="form-control form-control-sm"
+                                          id="colFormLabelSm" placeholder={user.lastName}/>}
+
+                              </div>
+                         </div>
                         }
+
+                        {user &&
+                         <div className="form-group row">
+                             <label htmlFor="colFormLabelSm"
+                                    className="col-sm-4 col-form-label col-form-label-sm">
+                                 Email
+
+                             </label>
+
+                              <div className="col-sm-8">
+                                  {!state.isEditing &&
+                                  <input readOnly type="email" className="form-control form-control-sm"
+                                         id="colFormLabelSm" placeholder={user.email}/>}
+                                  {state.isEditing &&
+                                   <input type="email" className="form-control form-control-sm"
+                                          id="colFormLabelSm" placeholder={user.email}/>}
+                              </div>
+                         </div>
+                        }
+                        {state.isEditing && user &&
+                         <div className="form-group row">
+                             <label htmlFor="colFormLabelSm"
+                                    className="col-sm-4 col-form-label col-form-label-sm">
+                                 Password
+                             </label>
+                             <div className="col-sm-8">
+                                  <input type="password"
+                                         className="form-control form-control-sm"
+                                         id="colFormLabelSm" placeholder={user.password}/>
+                             </div>
+                         </div>
+                        }
+
                         {user &&
                          <div className="form-group row">
                              <label htmlFor="colFormLabelSm"
@@ -62,16 +106,29 @@ export default withUser(props => {
                                  Birthday
                              </label>
                              <div className="col-sm-8">
-                                 <input readOnly type="date"
+                                 <input readOnly type="text"
                                         className="form-control form-control-sm"
                                         id="colFormLabelSm" placeholder={user.dob}/>
                              </div>
                          </div>
                         }
+
                     </form>
-                    {user &&
-                     <button className="btn btn-secondary btn-block">EDIT PROFILE</button>
+                    {!state.isEditing && user &&
+                     <button className="btn btn-secondary btn-block"
+                             //onClick={() => this.setState({isEditing: true})}
+                         >
+                         EDIT PROFILE
+                     </button>
                     }
+                    {state.isEditing && user &&
+                     <button className="btn btn-secondary btn-block"
+                             //onClick={() => this.setState({isEditing: false})}
+                         >
+                         UPDATE PROFILE
+                     </button>
+                    }
+
                 </div>
             </div>
 
@@ -80,3 +137,6 @@ export default withUser(props => {
     )
 
 });
+
+
+
