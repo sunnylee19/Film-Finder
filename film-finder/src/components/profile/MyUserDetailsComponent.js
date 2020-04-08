c3236e26784d7bcea38cea5d9e2749fdcf751f80
Ming-Shan (Sunny) Lee <sunnysmithie@gmail.com>
2020-04-08 21:23:17 +0800

    user email working, but user password and dob not showing

--- film-finder/src/components/profile/MyUserDetailsComponent.js
+++ film-finder/src/components/profile/MyUserDetailsComponent.js
@@ -1,57 +1,82 @@
-import React from 'react';
-import MyUserFollowingComponent from "./MyUserFollowingComponent";
-import withUser from "../../common/withUser";
+import React, { useState } from 'react';
+import '../../css/sign-in-css.css';
+import withUser from '../../common/withUser';
+import { logout } from '../../services/UserService';
 
-class MyUserDetailsComponent extends React.Component {
+export default withUser(props => {
 
-    render() {
-        return (
-            <div>
-                <div className="container card user-box col-4 float-left" >
-                    <img className="user-photo" src="https://avatars0.githubusercontent.com/u/1682522?s=400&u=5616079a6e3baa83b765695adce7515ac1749114&v=4" alt="Card image cap"/>
-                    <div className="card-body">
-                        <h5 className="card-title text-center">Jose</h5>
-                        <form>
-                            <div className="form-group row">
-                                <label htmlFor="colFormLabelSm"
-                                       className="col-sm-4 col-form-label col-form-label-sm">
-                                    Email
-                                </label>
-                                <div className="col-sm-8">
-                                    <input readOnly type="email" className="form-control form-control-sm"
-                                           id="colFormLabelSm" placeholder="reviewer@northeastern.edu"
 
-                                    />
-                                </div>
-                            </div>
+    const {user} = props;
+
+    const logoutHandler = async () => {
+        await logout();
+        props.setUser(null);
+    };
+
+
+    return (
+
+        <div>
+            <div className="container card user-box col-4 float-left" >
+                <img className="user-photo" src="https://avatars0.githubusercontent.com/u/1682522?s=400&u=5616079a6e3baa83b765695adce7515ac1749114&v=4" alt="Card image cap"/>
+                <div className="card-body">
+
+                    <h5 className="card-title text-center">
+                        Jose
+
+                    </h5>
+
+
+                    <form>
+                        {user &&
+                        <div className="form-group row">
+                            <label htmlFor="colFormLabelSm"
+                                   className="col-sm-4 col-form-label col-form-label-sm">
+                                Email
+
+                            </label>
+                            {user &&
+                            <div className="col-sm-8">
+                                <input readOnly type="email" className="form-control form-control-sm"
+                                       id="colFormLabelSm" placeholder={user.email}/>
+                            </div>}
+                        </div>
+                        }
+                        {user &&
                             <div className="form-group row">
                                 <label htmlFor="colFormLabelSm"
                                        className="col-sm-4 col-form-label col-form-label-sm">
                                     Password
                                 </label>
                                 <div className="col-sm-8">
-                                    <input readOnly type="password" className="form-control form-control-sm"
-                                           id="colFormLabelSm" placeholder=""/>
+                                    <input readOnly type="text"
+                                           className="form-control form-control-sm"
+                                           id="colFormLabelSm" placeholder={user.password}/>
                                 </div>
                             </div>
-                            <div className="form-group row">
-                                <label htmlFor="colFormLabelSm"
-                                       className="col-sm-4 col-form-label col-form-label-sm">
-                                    Birthday
-                                </label>
-                                <div className="col-sm-8">
-                                    <input readOnly type="date" className="form-control form-control-sm"
-                                           id="colFormLabelSm" placeholder="04/08/1998"/>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        }
+                        {user &&
+                         <div className="form-group row">
+                             <label htmlFor="colFormLabelSm"
+                                    className="col-sm-4 col-form-label col-form-label-sm">
+                                 Birthday
+                             </label>
+                             <div className="col-sm-8">
+                                 <input readOnly type="date"
+                                        className="form-control form-control-sm"
+                                        id="colFormLabelSm" placeholder={user.dob}/>
+                             </div>
+                         </div>
+                        }
+                    </form>
+                    {user &&
+                     <button className="btn btn-secondary btn-block">EDIT PROFILE</button>
+                    }
                 </div>
-
-
             </div>
-        )
-    }
-}
 
-export default withUser(MyUserDetailsComponent);
+
+        </div>
+    )
+
+});
