0ce82f7e975c9390b21655fdc116b23ca4071598
Jason Lu <jasonlu6@bu.edu>
2020-04-07 21:36:42 -0400

    Made changes to package-lock.json and package.json

---
+++ film-finder/src/components/privacy/PrivacyComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class PrivacyComponent extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Privacy Policy: </h2>
+
+                To be determined.
+            </div>
+        )
+    }
+}
+
+export default PrivacyComponent;
