7559763a53893e732ce2388d42f1df0a136b36ef
bachung <brandon.chung@vism.co>
2020-04-19 19:39:58 -0400

    Small fixes

--- film-finder/src/components/details/MovieCommentComponent.js
+++ film-finder/src/components/details/MovieCommentComponent.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import moment from 'moment';
 import {removeComment} from "../../services/CommentService";
-import '../../css/comment-moderation-css.css'
+import { Tooltip, OverlayTrigger } from 'react-bootstrap';
 
 const handleFlagComment = async () => {
     console.log("Comment flagged.");
 }
 
-const displayToolTip = async () => {
+const renderFlagTooltip = (props) => (
+    <Tooltip id="flag-tooltip" {...props}>
+        <span className="tooltiptext">
+            Flag comment for moderation
+        </span>
+    </Tooltip>
+);
 
-}
+const renderDeleteTooltip = (props) => (
+    <Tooltip id="delete-tooltip" {...props}>
+        <span className="tooltiptext">Remove comment.</span>
+    </Tooltip>
+);
 
 export default ({comment}) => (
     <div className="card" id={`c${comment.id}`}>
@@ -25,22 +35,17 @@ export default ({comment}) => (
             {comment.body}
             <p></p>
             <div className="row float-right">
-                <button className="btn btn-sm btn-warning" onClick={handleFlagComment}>
-                    <div className="tooltip">
-                    <span
-                        className="tooltiptext">Flag comment for moderation, given a violation of <a
-                        href="#">Terms of Service.</a>
-                    </span>
-                    </div>
-                    <h6>Flag&nbsp;&nbsp;<span></span><i className="fa fa-flag"></i></h6>
-                </button>
+                <OverlayTrigger overlay={renderFlagTooltip} placement="top">
+                    <button className="btn btn-sm btn-warning" onClick={handleFlagComment}>
+                        <h6>Flag&nbsp;&nbsp;<span></span><i className="fa fa-flag"></i></h6>
+                    </button>
+                </OverlayTrigger>
                 <span>&nbsp;</span>
-                <button className="btn btn-sm btn-danger" onClick={removeComment}>
-                    <div className="tooltip">
-                        <span className="tooltiptext">Remove comment.</span>
-                    </div>
-                    <h6>Delete&nbsp;&nbsp;<i className="fa fa-trash"></i></h6>
-                </button>
+                <OverlayTrigger overlay={renderDeleteTooltip} placement="top">
+                    <button className="btn btn-sm btn-danger" onClick={removeComment}>
+                        <h6>Delete&nbsp;&nbsp;<i className="fa fa-trash"></i></h6>
+                    </button>
+                </OverlayTrigger>
             </div>
         </div>
     </div>
