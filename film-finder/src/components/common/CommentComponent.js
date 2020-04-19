9d8ff15b842e0f7851abb0fdd68cd8f85ef5201b
Jason Lu <jasonlu6@bu.edu>
2020-04-19 19:03:04 -0400

    Revert back to Brandon's version of profile page.

--- film-finder/src/components/common/CommentComponent.js
+++ film-finder/src/components/common/CommentComponent.js
@@ -18,33 +18,3 @@ export default (props) => {
         </li>
     );
 };
-
-import React from 'react';
-import {postComment} from "../../services/CommentService";
-import moment from "moment";
-
-class CommentComponent extends React.Component {
-
-    /* To Do: figure out this part to add user followers */
-    addComment = () => {
-        postComment()
-    }
-
-    render() {
-        return (
-            <div>
-                <ul>
-                    <li class="border border-dark">
-                        <p>Zootopia- "This is a really good movie, must watch!"
-                        {moment().calendar().day}</p>
-                    </li>
-
-                </ul>
-
-                <br/>
-            </div>
-        )
-    }
-}
-
-export default CommentComponent;
